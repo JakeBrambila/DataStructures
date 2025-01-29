ca72b9e8f938617ddd1c26ed4b00a986a42a05f9
Jake <jake@proteatech.dev>
2025-01-29 14:18:22 -0800

    Add more methods to Singly Linked List and tests

--- Lists/SinglyLinkedList/SinglyLinkedList.go
+++ Lists/SinglyLinkedList/SinglyLinkedList.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // node of a linked list
 type Node struct {
@@ -53,4 +55,103 @@ func (l *LinkedList) Print() {
 		temp = temp.Next
 	}
 	fmt.Println(temp.Value)
+}
+
+func (l *LinkedList) InsertAt(index int, n *Node) {
+	//add to beginning
+	if index == 0 {
+		l.AddToBeginning(n)
+		return
+	}
+
+	//add to end
+	if index == l.Length-1 {
+		l.AddToEnd(n)
+		return
+	}
+
+	//inserts at any other index in the list
+	temp := l.Head
+	for i := 0; i < index-1; i++ {
+		temp = temp.Next
+	}
+	n.Next = temp.Next
+	temp.Next = n
+	l.Length++
+}
+
+// removes a node from the end of a Linked List
+func (l *LinkedList) RemoveEnd() {
+	temp := l.Head
+	for temp.Next != l.Tail {
+		temp = temp.Next
+	}
+	l.Tail = temp
+	l.Length--
+}
+
+// removes a node from the beginning of a Linked List
+func (l *LinkedList) RemoveBeginning() {
+	l.Head = l.Head.Next
+	l.Length--
+}
+
+func (l *LinkedList) RemoveAt(index int) {
+	//remove from beginning
+	if index == 0 {
+		l.RemoveBeginning()
+		return
+	}
+
+	//remove from end
+	if index == l.Length-1 {
+		l.RemoveEnd()
+		return
+	}
+
+	//removes from any other index
+	temp := l.Head
+	for i := 0; i < index-1; i++ {
+		temp = temp.Next
+	}
+	temp.Next = temp.Next.Next
+	l.Length--
+}
+
+// checks if a value is in a linked list
+func (l *LinkedList) IsInList(value int) bool {
+	temp := l.Head
+	for temp != l.Tail {
+		if temp.Value == value {
+			return true
+		}
+		temp = temp.Next
+	}
+
+	//check tail
+	if l.Tail.Value == value {
+		return true
+	}
+	return false
+}
+
+// removes a node in the list by the value
+func (l *LinkedList) RemoveValue(value int) {
+	if l.IsInList(value) {
+
+		//check if the value is the first Node
+		if l.Head.Value == value {
+			l.RemoveBeginning()
+			l.Length--
+		}
+
+		temp := l.Head
+		for temp.Next.Value != value {
+			temp = temp.Next
+		}
+		temp.Next = temp.Next.Next
+		l.Length--
+	} else {
+		fmt.Println("Value not in List.")
+	}
 }
--- Lists/SinglyLinkedList/main.go
+++ Lists/SinglyLinkedList/main.go
@@ -1,25 +1,81 @@
 package main
 
+import "fmt"
+
 func main() {
-	l := LinkedList{}
+	// Create a new linked list
+	ll := LinkedList{}
 
-	Node1 := &Node{Value: 50}
-	l.AddToEnd(Node1)
+	// Test AddToBeginning
+	fmt.Println("Testing AddToBeginning:")
+	n1 := &Node{Value: 10}
+	n2 := &Node{Value: 20}
+	ll.AddToBeginning(n1)
+	ll.AddToBeginning(n2)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 2
 
-	l.Print()
+	// Test AddToEnd
+	fmt.Println("\nTesting AddToEnd:")
+	n3 := &Node{Value: 30}
+	n4 := &Node{Value: 40}
+	ll.AddToEnd(n3)
+	ll.AddToEnd(n4)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 4
 
-	Node2 := &Node{Value: 30}
-	Node3 := &Node{Value: 20}
-	Node4 := &Node{Value: 60}
-	Node5 := &Node{Value: 90}
+	// Test InsertAt
+	fmt.Println("\nTesting InsertAt:")
+	n5 := &Node{Value: 25}
+	ll.InsertAt(2, n5) // Inserting 25 at index 2
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 5
 
-	l.AddToBeginning(Node2)
-	l.AddToBeginning(Node3)
+	// Test InsertAt beginning
+	fmt.Println("\nTesting InsertAt (beginning):")
+	n6 := &Node{Value: 5}
+	ll.InsertAt(0, n6)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 6
 
-	l.Print()
+	// Test InsertAt end
+	fmt.Println("\nTesting InsertAt (end):")
+	n7 := &Node{Value: 50}
+	ll.InsertAt(ll.Length-1, n7)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 7
 
-	l.AddToEnd(Node4)
-	l.AddToEnd(Node5)
+	// Test RemoveAt
+	fmt.Println("\nTesting RemoveAt:")
+	ll.RemoveAt(2) // Removing the node at index 2
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 6
 
-	l.Print()
+	// Test RemoveAt beginning
+	fmt.Println("\nTesting RemoveAt (beginning):")
+	ll.RemoveAt(0)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 5
+
+	// Test RemoveAt end
+	fmt.Println("\nTesting RemoveAt (end):")
+	ll.RemoveAt(ll.Length - 1)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 4
+
+	// Test IsInList
+	fmt.Println("\nTesting IsInList:")
+	fmt.Println(ll.IsInList(25))  // Expected Output: true
+	fmt.Println(ll.IsInList(100)) // Expected Output: false
+
+	// Test RemoveValue
+	fmt.Println("\nTesting RemoveValue:")
+	ll.RemoveValue(25)
+	ll.Print()
+	fmt.Println("Length:", ll.Length) // Expected Output: 3
+
+	// Test RemoveValue for non-existent value
+	fmt.Println("\nTesting RemoveValue (non-existent value):")
+	ll.RemoveValue(100)               // Expected Output: "Value is not in List."
+	fmt.Println("Length:", ll.Length) // Expected Output: 3
 }
