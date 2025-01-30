433a3624df76fad464d92e6684c20aa0b70b75b2
Jake <jake@proteatech.dev>
2025-01-29 23:50:22 -0800

    Add another method and test for doubly linked list

--- Lists/DoublyLinkedList/DoublyLinkedList.go
+++ Lists/DoublyLinkedList/DoublyLinkedList.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Value int
 	Next  *Node
@@ -25,10 +27,32 @@ func (l *LinkedList) AddBeginning(n *Node) {
 	}
 }
 
+func (l *LinkedList) PrintList() {
+	temp := l.Head
+	for temp != l.Tail {
+		fmt.Printf("%d -> ", temp.Value)
+		temp = temp.Next
+	}
+	fmt.Printf("%d", temp.Value)
+}
+
+func (l *LinkedList) PrintListReverse() {
+	temp := l.Tail
+	for temp != l.Head {
+		fmt.Printf("%d -> ", temp.Value)
+		temp = temp.Prev
+	}
+	fmt.Printf("%d", temp.Value)
+}
+
 func (l *LinkedList) AddToEnd(n *Node) {
 	if l.Head == nil {
 		l.Head = n
 		l.Tail = n
 		l.Length++
 	}
+	l.Tail.Next = n
+	n.Prev = l.Tail
+	l.Tail = n
+	l.Length++
 }
--- Lists/DoublyLinkedList/main.go
+++ Lists/DoublyLinkedList/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	// Create a new linked list
+	list := LinkedList{}
+
+	// Test adding nodes to the beginning of the list
+	node1 := &Node{Value: 10}
+	node2 := &Node{Value: 20}
+	node3 := &Node{Value: 30}
+
+	list.AddBeginning(node1)
+	list.AddBeginning(node2)
+	list.AddBeginning(node3)
+
+	// Print the list from head to tail
+	fmt.Println("Linked List (Head to Tail):")
+	list.PrintList()
+
+	fmt.Println()
+
+	// Print the list from tail to head
+	fmt.Println("Linked List (Tail to Head):")
+	list.PrintListReverse()
+
+	fmt.Println()
+
+	// Print the length of the list
+	fmt.Printf("Length of the list: %d\n", list.Length)
+
+	// Test adding nodes to the end of the list
+	node4 := &Node{Value: 40}
+	node5 := &Node{Value: 50}
+	node6 := &Node{Value: 60}
+
+	list.AddToEnd(node4)
+	list.AddToEnd(node5)
+	list.AddToEnd(node6)
+
+	// Print the list from head to tail
+	fmt.Println("Linked List after adding to the end (Head to Tail):")
+	list.PrintList()
+
+	fmt.Println()
+
+	// Print the list from tail to head
+	fmt.Println("Linked List after adding to the end (Tail to Head):")
+	list.PrintListReverse()
+
+	fmt.Println()
+
+	// Print the length of the list
+	fmt.Printf("Length of the list: %d\n", list.Length)
 }
